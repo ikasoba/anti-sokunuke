eb8979c4eb1e1a06251c69fcd1ce8a47d28e3103
ikasoba <57828948+ikasoba@users.noreply.github.com>
2023-05-22 22:41:54 +0900

    f

--- src/index.ts
+++ src/index.ts
@@ -1,26 +1,60 @@
-import fs from "fs/promises"
-import {Client} from "discord.js"
+import fs from "fs/promises";
+import { Client } from "discord.js";
+
+let o: { [k: string]: { name: string; guild: string; date: number } } = {};
+
+await fs
+  .readFile(".tmp.json", "utf-8")
+  .then((v) => (o = JSON.parse(v)))
+  .catch(() => fs.writeFile(".tmp.json", JSON.stringify(o), "utf-8"));
 
 interface Config {
-  duration: number
-  token: string
+  unbanDuration: number;
+  duration: number;
+  token: string;
 }
 
-const config: Config = JSON.parse(await fs.readFile("./config.json", "utf-8"))
+const config: Config = JSON.parse(await fs.readFile("./config.json", "utf-8"));
 
 const client = new Client({
-  intents: [ "GuildBans", "GuildMembers" ]
-})
+  intents: ["GuildBans", "GuildMembers"],
+});
 
 // 5 minutes
-const duration = config.duration * 1000
+const duration = config.duration * 1000;
 
-client.on("guildMemberRemove", member => {
-  console.log("leaved member", member.user.tag, "at", new Date().toLocaleString())
-  if ((Date.now() - (member.joinedTimestamp ?? Date.now())) < duration){
-    member.ban()
-    console.log("banned member", member.user.tag)
+client.on("guildMemberRemove", (member) => {
+  console.log(
+    "leaved member",
+    member.user.tag,
+    "at",
+    new Date().toLocaleString()
+  );
+  if (Date.now() - (member.joinedTimestamp ?? Date.now()) < duration) {
+    member.ban();
+    console.log("banned member", member.user.tag);
+    o[member.id] = {
+      date: Date.now() + config.unbanDuration,
+      guild: member.guild.id,
+      name: member.user.tag,
+    };
   }
-})
+});
 
-client.login(config.token).then(() => console.log("bot started."))
+const f = async () => {
+  for (const [k, v] of Object.entries(o)) {
+    if (v.date >= Date.now()) {
+      const guild = await client.guilds.fetch(o.guild);
+      await guild.members.unban(k);
+      console.log(`unbanned user ${v.name}`);
+      delete o[k];
+      fs.writeFile(".tmp.json", JSON.stringify(o), "utf-8");
+    }
+  }
+  setTimeout(f, 16);
+};
+
+client.login(config.token).then(() => {
+  console.log("bot started.");
+  f();
+});
